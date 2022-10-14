81088c3ca2af45230d2a7ae900329ef16268b8fe
RaunakScarlet <86217548+RaunakScarlet@users.noreply.github.com>
2022-10-14 23:38:11 +0530

    Time: 2261 ms (11.65%), Space: 294.8 MB (64.88%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -11,39 +11,19 @@
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        if (head -> next == nullptr)
-            return nullptr;
+       
         
-        int count = 0;
-        ListNode *p1 = head, *p2 = head;
-        
-        // First pass, count the number of nodes in the linked list using 'p1'.
-        while (p1 != nullptr) {
-            count++;
-            p1 = p1 -> next;
-        }
-        
-        // Get the index of the node to be deleted.
-        int middleIndex = count / 2;
-        
-        // Second pass, let 'p2' move toward the predecessor of the middle node.
-        for (int i = 0; i < middleIndex - 1; ++i)
-            p2 = p2 -> next;
-        
-        // Delete the middle node and return 'head'.
-        p2 -> next = p2 -> next -> next;
-        return head; 
-//         if(head==nullptr || head->next==nullptr) return nullptr;
-//         ListNode* fast=head;
-//         ListNode*slow=head;
-//         ListNode*temp;
-//         while(fast!=nullptr && fast->next!=nullptr){
-//             temp=slow;
-//             fast=fast->next->next;
+        if(head==nullptr || head->next==nullptr) return nullptr;
+        ListNode* fast=head;
+        ListNode*slow=head;
+        ListNode*temp;
+        while(fast!=nullptr && fast->next!=nullptr){
+            temp=slow;
+            fast=fast->next->next;
             
-//             slow=slow->next;
-//         }
-//         temp->next=temp->next->next;
-//         return head;
+            slow=slow->next;
+        }
+        temp->next=temp->next->next;
+        return head;
     }
 };
